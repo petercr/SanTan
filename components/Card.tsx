dde8f1af0ce8b93236eddbc55a9b2b749c3d465a
Peter Cruckshank <petecruckshank@gmail.com>
2022-08-05 17:35:23 -0400

    ✨ init Card component with CSS

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,12 +2,15 @@ import type { NextComponentType } from 'next';
 
 const Card: NextComponentType = () => (
   <div>
-    <h1>This is a card</h1>
-    <p>With some text</p>
+    <h1>Cape Cod World</h1>
+    <p>Bringing Your Ideas To The World 🌎</p>
     <style jsx>{`
       div {
-        color: red;
-        background: blue;
+        background: rgba(255, 255, 255, 0.81);
+        border: 1px solid #000000;
+        box-shadow: 8px 7px 8px rgba(0, 0, 0, 0.29);
+        border-radius: 20px;
+        padding: 1rem;
       }
     `}</style>
   </div>
