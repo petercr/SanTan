0adbb279c4f74ef5071047499f9f7fdca55b983c
Peter Cruckshank <petecruckshank@gmail.com>
2022-12-28 21:21:00 -0500

    ✨ got the /design page pretty much working! 🥳

--- src/pages/design.tsx
+++ src/pages/design.tsx
@@ -3,12 +3,23 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styled from 'styled-components';
+import styles from '../../styles/Home.module.css';
 import { StyledDiv } from '../components/Card';
 import HeaderCard from '../components/HeaderCard';
 import SocialLinksCard from '../components/SocialLinksCard';
 
+const StyledPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: sticky;
+  z-index: 10;
+  margin-top: 5vh;
+`;
+
 const Design: NextPage = () => (
-  <div>
+  <div className={styles.container}>
     <Head>
       <title>Cape Cod World</title>
       <meta name="description" content="A website starts with a design" />
@@ -16,13 +27,29 @@ const Design: NextPage = () => (
     </Head>
 
     <main>
-      <div>
+      <StyledPage>
         <HeaderCard text="Design" typeElm="h1" />
         <StyledDiv>
-          <Image src="/undraw_designer_re_5v95.svg" />
+          <Image
+            src="/undraw_designer_re_5v95.svg"
+            layout="fixed"
+            height={100}
+            width={100}
+          />
+          <p>
+            Ullamcorper sagittis neque diam magna. Viverra augue nibh
+            pellentesque neque fames nunc tortor vivamus venenatis. Dui
+            facilisis diam nibh viverra in nunc, in tellus pellentesque. Aliquam
+            placerat eget ut felis eu tempus. Commodo, tristique consectetur
+            egestas duis risus orci lorem magna. Imperdiet cras eu pretium
+            volutpat duis fermentum lorem. Aliquet semper pellentesque tincidunt
+            at. Ultricies fringilla risus pellentesque neque diam id. Risus
+            volutpat duis malesuada sit. Vel mauris, hendrerit tellus mollis mi
+            nunc.
+          </p>
         </StyledDiv>
         <SocialLinksCard />
-      </div>
+      </StyledPage>
       <Image
         src="/beach_backdrop.svg"
         alt="beach drawing"
