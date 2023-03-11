7a64097057fc247190923e63fd5773e7a5c154ce
Peter Cruckshank <petecruckshank@gmail.com>
2023-03-11 17:50:22 -0500

    ✨ init new Contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from 'next';
+import Head from 'next/head';
+import styles from '../../styles/Home.module.css';
+
+const Contact: NextPage = () => (
+  <div className={styles.container}>
+    <Head>
+      <title>Contact | Cape Cod World</title>
+      <meta name="description" content="A website starts with a design" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <div>hello</div>;
+  </div>
+);
+
+export default Contact;
