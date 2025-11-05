3386dfee43e870cceb9046ba9dc06070d2df7ec5
magnehustveit <magne.hustveit@spk.no>
2025-11-06 00:45:07 +0100

    chore: small fixes

--- apps/studio/sanity.cli.ts
+++ apps/studio/sanity.cli.ts
@@ -2,7 +2,7 @@ import { defineCliConfig } from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
-    projectId: '88hgbtze',
+    projectId: 'qzo347ei',
     dataset: 'production',
   },
   deployment: {
